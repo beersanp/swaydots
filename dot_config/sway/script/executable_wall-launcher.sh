f9f897be2b7de284fe334256d806c1e4f5a6da92
Chitsanupong Chancluab <beersanp@duck.com>
2024-07-28 20:27:43 +0700

    wall-launcher / gitnuro (windowsrule)

--- dot_config/sway/script/executable_wall-launcher.sh
+++ dot_config/sway/script/executable_wall-launcher.sh
@@ -18,6 +18,7 @@ if [[ -n "$selected_file" ]]; then
   # generate wal -> change wallpaper with swaybg
   wal -i "$selected_file"
   bash $HOME/.config/sway/script/wallpaper-script/apply-theme.sh
+  bash $HOME/.config/sway/script/zed-theme-wal/generate_theme
   sway reload
   notify-send "Wallpaper Changed" "Successfully set '$selected_file' as wallpaper\n recheck GTK/QT Theme"
 else
